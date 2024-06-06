13901b9963ffdc6f2a66c0d081b1c90f8ed49988
Mark Kogon <mlkogon231@gmail.com>
2024-06-06 10:40:55 -0700

    terraform files

---
+++ terraform/variables.tf
@@ -0,0 +1,16 @@
+# variable instance_ami {
+#     default = "ami-0bfac9aa66a558bd8"
+# } 
+
+# variable instance_type {
+#     default = "t2.micro"
+# }
+
+# variable key_name {
+#     default = "portfolio"
+# }
+
+# variable security_group {
+#     default = "portfolio-sg"
+# }
+  
